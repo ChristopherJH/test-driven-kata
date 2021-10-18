7fdeb33d80cafa203e7559fcc5defabd9c78604b
Christopher Hallett <chrisjameshal@gmail.com>
2021-10-18 12:13:49 +0100

    Format and lint

--- src/skeleton.test.ts
+++ src/skeleton.test.ts
@@ -5,21 +5,27 @@ test("Zero people test:", () => {
 });
 
 test("One person test:", () => {
-  expect(whoLikesIt(['Josh'])).toBe("Josh likes this");
+  expect(whoLikesIt(["Josh"])).toBe("Josh likes this");
 });
 
 test("Two person test:", () => {
-  expect(whoLikesIt(['Josh', 'Chris'])).toBe("Josh and Chris like this");
+  expect(whoLikesIt(["Josh", "Chris"])).toBe("Josh and Chris like this");
 });
 
 test("Three person test:", () => {
-  expect(whoLikesIt(['Josh', 'Chris', 'Tim'])).toBe("Josh, Chris and Tim like this");
+  expect(whoLikesIt(["Josh", "Chris", "Tim"])).toBe(
+    "Josh, Chris and Tim like this",
+  );
 });
 
 test("Four person test:", () => {
-  expect(whoLikesIt(['Josh', 'Chris', 'Tim', 'Jess'])).toBe("Josh, Chris and 2 others like this");
+  expect(whoLikesIt(["Josh", "Chris", "Tim", "Jess"])).toBe(
+    "Josh, Chris and 2 others like this",
+  );
 });
 
 test("Five person test:", () => {
-  expect(whoLikesIt(['Josh', 'Chris', 'Tim', 'Jess', 'Jennifer'])).toBe("Josh, Chris and 3 others like this");
+  expect(whoLikesIt(["Josh", "Chris", "Tim", "Jess", "Jennifer"])).toBe(
+    "Josh, Chris and 3 others like this",
+  );
 });
--- src/skeleton.ts
+++ src/skeleton.ts
@@ -6,20 +6,18 @@
 function whoLikesIt(people: string[]): string {
   if (people.length === 0) {
     return "no one likes this";
-  }
-  else if (people.length === 1) {
+  } else if (people.length === 1) {
     return `${people[0]} likes this`;
-  }
-  else if (people.length === 2) {
+  } else if (people.length === 2) {
     return `${people[0]} and ${people[1]} like this`;
-  }
-  else if (people.length === 3) {
+  } else if (people.length === 3) {
     return `${people[0]}, ${people[1]} and ${people[2]} like this`;
+  } else {
+    return `${people[0]}, ${people[1]} and ${
+      people.length - 2
+    } others like this`;
   }
-  else {
-    return `${people[0]}, ${people[1]} and ${people.length-2} others like this`;
-  }
-  return 'wrong';
+  return "wrong";
 }
 
 export default whoLikesIt;
