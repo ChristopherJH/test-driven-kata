ab5d234a05357678f854ba3fe753805a0433b1b1
Christopher Hallett <chrisjameshal@gmail.com>
2021-10-18 12:08:00 +0100

    Add more test cases

--- src/skeleton.test.ts
+++ src/skeleton.test.ts
@@ -16,6 +16,10 @@ test("Three person test:", () => {
   expect(whoLikesIt(['Josh', 'Chris', 'Tim'])).toBe("Josh, Chris and Tim like this");
 });
 
-test("Three person test:", () => {
+test("Four person test:", () => {
   expect(whoLikesIt(['Josh', 'Chris', 'Tim', 'Jess'])).toBe("Josh, Chris and 2 others like this");
+});
+
+test("Five person test:", () => {
+  expect(whoLikesIt(['Josh', 'Chris', 'Tim', 'Jess', 'Jennifer'])).toBe("Josh, Chris and 3 others like this");
 });
